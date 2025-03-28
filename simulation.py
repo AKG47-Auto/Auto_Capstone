be12c6a89c185b105b651c7011f9c801618fa07b
Aayush Gupta <aayushkg@umich.edu>
2025-03-27 20:11:33 -0400

    Updated Simulation Code

--- simulation.py
+++ simulation.py
@@ -22,27 +22,15 @@ while True:
     obs, reward, done, info = vec_env.step(action)
     print("Observation shape:", obs.shape)
     print("Observation:", obs)
-    #lookahead = org_env.get_lookahead_waypoint()
-    # waypoint = org_env.get_waypoint()
-
-    # #print("lookahead:", lookahead)
-    # print("current waypoint:", waypoint)
-    # x = obs[0][0]
-    # y = obs[0][1]
-    # vx = obs[0][2]
-    # vy = obs[0][3]
-    # velocity = vx
-    # print(f"Location: {location[0]}, {location[1]}")
-    # print(f"Velocity: {velocity}")
-    # obs = np.squeeze(obs)
+    
     mujoco.mj_step(vec_env.envs[0].model, vec_env.envs[0].data)
     org_env.viewer.sync()
     time.sleep(0.006)
 
-    # if done:
-    #     print("Episode done. Resetting environment...")
-    #     org_env.save_state_history("state_history_episode_straight.csv")
-    #     obs = vec_env.reset()
+    if done:
+        print("Episode done. Resetting environment...")
+        org_env.save_state_history("state_history_episode_straight.csv")
+        obs = vec_env.reset()
         
-    #     # obs = np.squeeze(obs)
-    #     break
+        # obs = np.squeeze(obs)
+        break
